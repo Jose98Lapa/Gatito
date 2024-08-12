899a8b123b84425226ce1fbc86ddc6e62d04ce76
José Lapa <josemmlapa@gmail.com>
2024-08-12 23:17:52 +0100

    Updated Connection Helper to observable obejct

--- Gatito/Gatito/Helpers/ConnectionHelper.swift
+++ Gatito/Gatito/Helpers/ConnectionHelper.swift
@@ -9,31 +9,19 @@ import Foundation
 import SystemConfiguration
 import WebKit
 
-class ConnectionHelper {
+class ConnectionHelper: ObservableObject {
     
-    static func isConnectionAvailable() -> Bool {
-        
-        var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-        
-        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
-                SCNetworkReachabilityCreateWithAddress(nil, $0)
-            }
-        }) else {
-            return false
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue.global(qos: .background)
+    @Published var isNetworkAccessible: Bool = false
+    
+    init() {
+        setupPathHandler()
+    }
+    
+    func setupPathHandler() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.isNetworkAccessible = path.status == .satisfied
         }
-        
-        var flags: SCNetworkReachabilityFlags = []
-        
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
-            return false
-        }
-        
-        let isReachable = flags.contains(.reachable)
-        let needsConnection = flags.contains(.connectionRequired)
-        
-        return isReachable && !needsConnection
     }
 }
