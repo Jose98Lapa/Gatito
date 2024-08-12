c8db5315e5794f44f891234d44262dd7e0c355c9
José Lapa <josemmlapa@gmail.com>
2024-08-12 20:59:56 +0100

    Created connection helper to track internet connectivity status

---
+++ Gatito/Gatito/Helpers/ConnectionHelper.swift
@@ -0,0 +1,39 @@
+//
+//  ConnectionHelper.swift
+//  Gatito
+//
+//  Created by José Miguel Lapa on 12/08/2024.
+//
+
+import Foundation
+import SystemConfiguration
+import WebKit
+
+class ConnectionHelper {
+    
+    static func isConnectionAvailable() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return isReachable && !needsConnection
+    }
+}
