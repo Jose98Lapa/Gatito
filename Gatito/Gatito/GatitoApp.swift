a5b594ddd3e4d2133ed10093abadb34dc2955667
José Lapa <josemmlapa@gmail.com>
2024-08-16 19:11:25 +0100

    Updated App to react to connection changes

--- Gatito/Gatito/GatitoApp.swift
+++ Gatito/Gatito/GatitoApp.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import Toast
 
 @main
 struct GatitoApp: App {
+    
     let persistenceController = PersistenceController.shared
-
+    @ObservedObject var connectionHelper = ConnectionHelper()
+    
+    let toast = Toast.default(
+        image: UIImage(systemName: "exclamationmark.triangle")!,
+        title: "Internet unavailable.",
+        config: ToastConfiguration(dismissBy: [.tap, .time(time: 10.0)])
+    )
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: connectionHelper.isNetworkAccessible) { _, isNowConnected in
+            if !isNowConnected { toast.show(haptic: .warning) }
         }
     }
 }
+
+
